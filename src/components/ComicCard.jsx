54e551c4968ee31ad5eecf137a8ad38a95d1e5c3
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-04-29 16:54:07 +0200

    fix: corretto nome proprietà immagine

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -1,7 +1,7 @@
 function ComicCard({ comic }) {
   return (
     <div className="comic-card">
-      <img src={comic.thumbnail} alt={comic.title} />
+      <img src={comic.thumb} alt={comic.title} />
       <h4>{comic.title}</h4>
     </div>
   );
